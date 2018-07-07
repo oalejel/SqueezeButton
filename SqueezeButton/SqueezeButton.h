8d208a3070ec280f16ce53134fe170ac3bb52b17
oalejel <alejel.omar@gmail.com>
2018-07-07 16:12:48 -0700

    Initial Commit

---
+++ SqueezeButton/SqueezeButton.h
@@ -0,0 +1,19 @@
+//
+//  SqueezeButton.h
+//  SqueezeButton
+//
+//  Created by Omar Al-Ejel on 7/7/18.
+//  Copyright © 2018 omaralejel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SqueezeButton.
+FOUNDATION_EXPORT double SqueezeButtonVersionNumber;
+
+//! Project version string for SqueezeButton.
+FOUNDATION_EXPORT const unsigned char SqueezeButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SqueezeButton/PublicHeader.h>
+
+
